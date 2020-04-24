ac0196ff9ed9a99d5014db9474ad5e46bd2638af
System Administrator <root@MacBook-Pro-Maxim.local>
2020-04-25 00:46:09 +0300

    :octocat:

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+export const TICK_RATE = 3000;
+export const ICONS = ["fish", "poop", "weather"];
+export const RAIN_CHANCE = 0.2;
+export const SCENES = ["day", "rain"];
+export const DAY_LENGHT = 60;
+export const NIGHT_LENGHT = 4;
+
+export const getNextHungerTime = clock =>
+  Math.floor(Math.random() * 3) + 5 + clock;
+export const getNextDieTime = clock =>
+  Math.floor(Math.random() * 2) + 3 + clock;
+export const getNextPoopTime = clock =>
+  Math.floor(Math.random() * 3) + 4 + clock;
